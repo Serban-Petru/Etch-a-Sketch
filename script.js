b55136382c76c4dff29d11768d36c0b8d4cf5915
Serban-Petru <177335339+Serban-Petru@users.noreply.github.com>
2024-12-14 20:50:15 +0200

    proiectel

---
+++ script.js
@@ -0,0 +1,37 @@
+const container = document.querySelector('.container');
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+
+
+
+function create_row(number_of_the_row){
+
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for(let i = 1 ; i <= number_of_the_row; i++){
+        const box = document.createElement('div');  
+        box.classList.add('box');
+        row.appendChild(box);
+        box.addEventListener("mouseover", () => {
+            box.style.background = 'orange';
+        });
+        
+    }
+    container.appendChild(row);
+  
+}
+
+function create_grid(size){ 
+    for(let i = 1 ; i <= size; i++){
+        create_row(size);
+    }
+}
+create_grid(5);
+button.addEventListener("click", () => {
+    while (container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+    create_grid(input.value);
+    
+})
