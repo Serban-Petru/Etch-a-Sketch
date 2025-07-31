2bd6a989ede19d79cb335cb8301fbdd6018c22fe
Serban-Petru <177335339+Serban-Petru@users.noreply.github.com>
2025-07-31 13:22:27 +0000

    canvas and sldier

--- javascript.js
+++ javascript.js
@@ -1,20 +1,38 @@
+let defaultSize_x = 2;
+let defaultSize_y = 2;
+
+
+function sliderData(){
+    const colectData = document.querySelector("#volume");
+    const showData = document.querySelector("#sliderData");
+    colectData.addEventListener("click",() => {
+    showData.textContent = colectData.value;
+    
+});
+}
 
 function createArray(arrayNumber,blockNumber){
-    for(let i = 0; i< arrayNumber; i++){
+    for(let i = 1; i<= arrayNumber; i++){
     const matrix = document.querySelector(".matrix");
     const array = document.createElement("ul");
     
-    array.setAttribute("id",`array-${arrayNumber}`);
+    array.setAttribute("id",`array-${i}`);
     matrix.appendChild(array);
-    for(let i = 0; i< blockNumber; i++){
+    for(let j = 1; j<= blockNumber; j++){
         const block = document.createElement("li");
         
-        block.setAttribute("id",`array-${blockNumber}`);
+        block.setAttribute("id",`array-${i}-${j}`);
         array.appendChild(block);
+        colorChange(block,"red");
     }
 }
 }
 
+function colorChange(block,color){
+    block.addEventListener("mouseenter",() => {
+        block.style.background = color;
+    });
+}
+sliderData();
+createArray(2,2);
 
-
-createArray(5,5);
